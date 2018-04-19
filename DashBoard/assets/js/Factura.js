f857fb68864d20580b8184f97cd7ab41b4c2887c
jaroja4 <jaroja4@gmail.com>
2018-04-19 07:55:41 -0600

    Factura de Productos

--- DashBoard/assets/js/Factura.js
+++ DashBoard/assets/js/Factura.js
@@ -22,47 +22,30 @@ class Factura {
 }
 
 let factura = new Factura();
+var UltProTou = ""; //Ultimo Producto Tocado -- Sirve para identificar cual fue el ultimo producto que se agrego o se modifico en la lista
 
 $(document).ready(function () {
-    // Load list
-    //LoadAll();
-    //LoadCategories();
-    //Form Validate
-    /*$('#frmProducto').Validate({
-        submitHandler: function() {
-            //$('#btnProducto').attr("disabled", "disabled");
-            Save();   
-        }
-    });*/
-    // eventos
-    $("tbody tr").click(function () {
-        $('.selected').removeClass('selected');
-        $(this).addClass("selected");
-        // var product = $('.p',this).html();
-        // var infRate =$('.i',this).html();
-        // var note =$('.n',this).html();
-        // alert(product +','+ infRate+','+ note);
-        alert("si entra")
-    });
 
-    $(".cantidad").change(function() {
-        // factura.cantidad= $('#cant_1').val();
-        // factura.precio= $('#precio_1').text();
-        // $('#importe1').text(factura.Importe);
-        alert ("entra");
-    });
 });
 
-$("#p_searh").keypress(function(e) {
+//Valida si se presiona enter -> carga el producto en la lista
+//si preciona "*" -> Quita el "*", pasa el punto de insercion a la cantidad y selecciona el valor para ser reemplazdo con el teclado
+$("#p_searh").keyup(function(e) {
     if(e.which == 13) {
        // Acciones a realizar, por ej: enviar formulario.
        LoadProducto();
+    }
+    if (e.which == 106) {
+        $("#p_searh")[0].value = "";
+        // $("#cant_"+ document.getElementById("productos").lastChild.childNodes[3].innerText).focus().select();;
+        // se usa UltProTou xq es el UltimoProductoTocado (si la cantidad del producto es alterada no hay forma de saber cual fue el ultimo modificado.)
+        $("#cant_"+ UltProTou).focus().select(); 
     }
  });
 
 
 
-// Carga lista
+// Carga los productos a la lista de la factura
 function LoadProducto() {
     factura.p_codigo = $("#p_searh").val();  //Columna 0 de la fila seleccionda= ID.
     $.ajax({
@@ -75,41 +58,168 @@ function LoadProducto() {
     })
     .done(function (e) {
         CleanCtls();
-        showDataProducto(e);
+        ValidateProductFac(e);
     })
     .fail(function (e) {
         showError(e);
     });
 };
+// valida que el producto nuevo a ingresar no este en la lista
+// si esta en la lista lo suma a la cantidad
+// si es nuevo lo agrega a la lista
+function ValidateProductFac(e){
+    //compara si el articulo ya existe
+    // carga lista con datos.
+    var data = JSON.parse(e)[0];
+    UltProTou = data.codigorapido;
+    var repetido = false;
 
+    if(document.getElementById("productos").rows.length != 0 && data != null){
+        $(document.getElementById("productos").rows).each(function(i,item){
+            // alert(item.childNodes[3].innerText);
+            if(item.childNodes[3].innerText==data.codigorapido){
+                item.childNodes[9].firstElementChild.value = parseFloat(item.childNodes[9].firstElementChild.value) + 1;
+                item.childNodes[11].firstChild.textContent = "¢" + parseFloat((item.childNodes[7].firstChild.textContent).replace("¢","")) * parseFloat(item.childNodes[9].firstElementChild.value);
+                repetido=true;
+                calcTotal();
+            }     
+        });
+    }    
+    if (repetido==false){
+        showDataProducto(e);
+    }
+};
+
+//Agrega el producto a la lista
 function showDataProducto(e) {
     // carga lista con datos.
     var data = JSON.parse(e);
     // Recorre arreglo.
     $.each(data, function (i, item) {
         var opt =
-        '<tr class="even pointer">' +
-            '<td class="a-center ">' +
-                '<div class="icheckbox_flat-green" style="position: relative;">' +    
-                    '<input type="checkbox" class="flat" name="table_records" style="position: absolute; opacity: 0;">' +
-                    '<ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins>' +
-                '</div>' +
-            '</td>' +
-            '<td class=" ">'+ item.codigorapido + '</td>' +
-            '<td class=" ">'+ item.descripcion + '</td>' +
-            '<td class=" " id="precio_'+ item.codigorapido + '">¢'+ item.precio+ '</td>' +
-            '<td class="">' +
-            '   <input class="cantidad form-control" type="number" min="1" max="9999" step="1"value="1" id="cant_'+ item.codigorapido + '">' +
-            '</td>' +
-                '<td id="importe_'+ item.codigorapido + '">¢'+ item.precio + '</td>' +
-        '</tr>';
-        // '<option value="'+ item.id + '">'+ item.nombre + '</option>';
+        `<tr class="even pointer">
+            <td class="a-center ">
+                <div class="icheckbox_flat-green" style="position: relative;">
+                    <input type="checkbox" class="flat" name="table_records" style="position: absolute; opacity: 0;">
+                    <ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins>
+                </div>
+            </td>
+            <td class="codigorapido" id="id_${item.codigorapido}"> ${item.codigorapido} </td>
+            <td class="descripcion">${item.descripcion}</td>
+            <td class=" " id="precio_${item.codigorapido}">¢${item.precio}</td>
+            <td class="">
+               <input class="cantidad form-control" type="number" min="1" max="999" step="1" value="1" id="cant_${item.codigorapido}" onchange="importe(${item.codigorapido})">
+            </td>
+                <td id="importe_${item.codigorapido}">¢${item.precio}</td>
+        </tr>`;
+        //<option value="'+ item.id + '">'+ item.nombre + '</option>';
         $('#productos').append(opt);
         // evento click del boton modificar-eliminar
         //$('#option' + item.id).click(event-handler);
     })
+    calcTotal();
 };
 
+//Calcula los totales cada vez que un producto es modificado
+function calcTotal(){
+    var subT=0;
+    $(document.getElementById("productos").rows).each(function(i,item){
+        // alert(item.childNodes[3].innerText);
+        
+        subT= subT + parseFloat((item.childNodes[11].textContent).replace("¢","")); 
+    });
+    $("#subtotal")[0].textContent = "¢"+subT.toFixed(2); 
+    $("#desc_val")[0].textContent = "¢"+ (subT * (parseFloat(($("#desc_100")[0].textContent).replace("%",""))) / 100).toFixed(2) ;
+    $("#iv_val")[0].textContent = "¢"+ ((subT * (parseFloat(($("#iv_100")[0].textContent).replace("%","")) / 100)) - (parseFloat(($("#desc_val")[0].textContent).replace("¢","")))).toFixed(2) ;
+    $("#total")[0].textContent = "¢" + ((($("#subtotal")[0].textContent).replace("¢","")) - parseFloat(($("#desc_val")[0].textContent).replace("¢","")) + parseFloat(($("#iv_val")[0].textContent).replace("¢",""))).toFixed(2);
+};
+
+
+//Calcula el nuevo importe al cambiar la cantidad del prodcuto seleccionado de forma manual y no por producto repetido.
+function importe(codigorapido){
+    UltProTou = codigorapido;
+    pUnit = $(`#precio_${codigorapido}`)[0].textContent.replace("¢","");
+    cant = $(`#cant_${codigorapido}`)[0].value;
+    $(`#importe_${codigorapido}`)[0].textContent = "¢" + (parseFloat(pUnit) * parseFloat(cant)).toString();
+    $(`#importe_${codigorapido}`)[0].textContent
+    $(`#cant_${codigorapido}`).keyup(function(e) {
+        if(e.which == 13) {
+           if (cant==0){
+            // alert("Borar Producto");
+            // $(`id_${codigorapido}`).parentElement
+            var rm = $(`id_${codigorapido}`).context.activeElement.parentElement.parentElement;
+            rm.remove();
+            calcTotal();
+           }
+            calcTotal();
+            $("#p_searh").focus();
+        }
+     });
+};
+
+$( "#fac-ccard" ).click(function(event) {
+    $("#formapago").empty();
+
+
+    var newDiv =
+    `<div class="row">
+        <div class="col-md-4 col-md-offset-4 col-sm-offset-0 col-xs-6 col-xs-offset-0">
+            <h3 class="text-left" >Ingrese Ref.:</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <input class="input-lg" type="text" placeholder="Ingrese Numero Referencia"  required="" minlength="5" autofocus="">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 col-sm-offset-0 col-xs-6 col-xs-offset-0">
+            <button type="button" class="btn btn-primary" style="margin-top:10px;">Procesar</button>
+        </div>
+    </div>`;
+    $("#formapago").append(newDiv);
+
+
+
+});
+
+
+$( "#fac-cash" ).click(function(event) {
+    $("#formapago").empty();
+
+
+    var newDiv =
+    `<div class="row">
+        <div class="col-md-4 col-md-offset-4 col-sm-offset-0 col-xs-6 col-xs-offset-0">
+            <h3 class="text-left" >Paga con:</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <input class="input-lg" type="text" placeholder="Ingrese Monto en Efectivo"  required="" minlength="5" autofocus="">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 col-sm-offset-0 col-xs-6 col-xs-offset-0">
+            <button type="button" class="btn btn-primary" style="margin-top:10px;">Procesar</button>
+        </div>
+    </div>`;
+    $("#formapago").append(newDiv);
+
+
+
+});
+
+
+function facturar(metodo){
+    alert("EFECTIVO");
+    if (metodo == "cash"){
+        alert("EFECTIVO");
+    }
+    if (metodo=="creditcard") {
+        alert ("TARJETA");
+    }
+};
 // Carga lista
 function LoadAll() {
     id=null;
